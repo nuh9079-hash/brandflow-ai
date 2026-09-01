a5d2ee48d1907d0b901db5404241bcc5a0ef582b
nuh9079-hash <nuh9079@gmail.com>
2026-09-01 21:43:30 +0300

    fix: allow Rocket preview when Clerk secret is unavailable

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import { auth } from "@clerk/nextjs/server";
 import CommandCenter from "./CommandCenter";
 
 export default async function Home() {
-  await auth?.protect();
+  const clerkEnabled = Boolean(
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY && process.env.CLERK_SECRET_KEY
+  );
+
+  if (clerkEnabled) await auth.protect();
+
   return <CommandCenter />;
 }
